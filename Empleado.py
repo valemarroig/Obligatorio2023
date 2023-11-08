6bd84411f304d7578de8c2915acd7aa2aa7570d2
williambeare <148827854+williambeare@users.noreply.github.com>
2023-11-08 18:05:06 -0300

    Update Empleado.py

--- Empleado.py
+++ Empleado.py
@@ -1,2 +1,23 @@
 class Empleado:
-  def __init__(self, id, nacionalidad
+    def __init__(self, id, nombre, fecha_nacimiento, nacionalidad, salario):
+        self._id = id
+        self._nombre = nombre
+        self._fecha_nacimiento = fecha_nacimiento
+        self._nacionalidad = nacionalidad
+        self._salario = salario
+
+    @property
+    def get_id(self):
+        return self._id
+
+    def get_nombre(self):
+        return self._nombre
+
+    def get_fecha_nacimiento(self):
+        return self._fecha_nacimiento
+
+    def get_nacionalidad(self):
+        return self._nacionalidad
+
+    def get_salario(self):
+        return self._salario
