633bdd61fb43128ef21dabb33060e403c871a7c3
valemarroig <148827741+valemarroig@users.noreply.github.com>
2023-11-08 19:10:04 -0300

    Piloto.py

--- Piloto.py
+++ Piloto.py
@@ -8,3 +8,42 @@ class Piloto(Empleado):
 
     def __str__(self):
         return f"Piloto: {self.nombre} (ID: {self.id}, Número de Auto: {self.num_auto}, Puntaje: {self.puntaje_campeonato}, Lesionado: {self.lesionado})"
+
+    @property
+    def get_score(self):
+        return self._score
+
+    @property
+    def get_num_auto(self):
+        return self._num_auto
+
+    @property
+    def get_puntaje_campeonato(self):
+        return self._puntaje_campeonato
+    
+    @property
+    def get_lesionado(self):
+        return self._lesionado
+
+    @property
+    def set_score(self, score):
+        return self._score=score
+
+    @property
+    def set_num_auto(self, num_auto):
+        self._num_auto= num_auto
+
+    @property
+    def set_puntaje_campeonato(self,puntaje_campeonato):
+        self._puntaje_campeonato=puntaje_campeonato):
+
+    @property
+    def set._lesionado(self, lesionado):
+        self._lesionado=lesionado
+
+    def __str__(self):
+        return f"Piloto: {self.nombre} (ID: {self.id}, Numero de Auto: {self._num_auto}, Puntaje: {self._puntaje_campeonato}, Lesionado: {self._lesionado})"
+        
+        
+    
+        
