9b50ac3d67d7d737324bb44fdd241df76cf1c56c
williambeare <wbeare@correo.um.edu.uy>
2023-11-10 19:16:32 -0300

    import

--- Mecanico.py
+++ Mecanico.py
@@ -1,3 +1,5 @@
+from Empleado import Empleado
+
 class Mecanico(Empleado):
     def __init__(self, id, nombre, fecha_nacimiento, nacionalidad, salario, score):
         super().__init__(id, nombre, fecha_nacimiento, nacionalidad, salario)
